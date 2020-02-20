412f25299bf71c466a230d04519a555c3e430b1a
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-20 21:03:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
 def reverse_each_word(string)
-  array = string.split(' ').each{|n| n = n.reverse}.collect{|n| n = n.reverse}join(' ')
+  array = string.split(' ').each{|n| n = n.reverse}.collect{|n| n = n.reverse}.join(' ')
 end 
